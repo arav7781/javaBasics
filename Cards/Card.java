c520f2993c10f5bd54738ed568361993c81cf9b2
arav7781 <aravsaxena884@gmail.com>
2025-02-04 10:50:45 +0530

    Cards

---
+++ Cards/Card.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Card{
+    String rank;
+    String suit;
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;    
+    }
+    public String toString(){
+        return rank + " of " + suit;
+    }
+}
