c9186e3a718912600776178b2250f3609837f2f5
arav7781 <aravsaxena884@gmail.com>
2025-07-01 08:52:20 +0530

    Try catch exception handling

---
+++ exception/tryCatch.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class tryCatch {
+    public static void main(String[] args) {
+    int j = 0;
+    int i = 0;
+    try{
+         j = 18/i;
+    }
+    catch(Exception e){
+        System.out.println(e);
+    }
+    }
+}
