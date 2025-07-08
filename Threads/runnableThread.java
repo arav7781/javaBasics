589135827e60621ea9d354fa26a800097445bb1c
arav7781 <aravsaxena884@gmail.com>
2025-07-08 07:08:23 +0530

    Thread Runnable interface

---
+++ Threads/runnableThread.java
@@ -0,0 +1,17 @@
+package Threads;
+
+public class runnableThread {
+    static class A implements Runnable{
+        public void run(){
+            for(int i = 0;i<5;i++){
+                System.out.println("hello");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Runnable obj1 = new A();
+        Thread t1 = new Thread(obj1);
+
+        t1.start();
+    }
+}
