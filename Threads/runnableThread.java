9fcf9cae9120b60a3d490dd86ba6584bd3e1245f
arav7781 <aravsaxena884@gmail.com>
2025-07-08 07:14:20 +0530

    Lmbda exp def using runnable as it has functional interface

--- Threads/runnableThread.java
+++ Threads/runnableThread.java
@@ -9,11 +9,9 @@ public class runnableThread {
         }
     }
     public static void main(String[] args) {
-        Runnable obj1 = new Runnable() {
-            public void run(){
-                for(int i = 0;i<5;i++){
-                    System.out.println("hello");
-                }
+        Runnable obj1 = () -> {
+            for(int i = 0;i<5;i++){
+                System.out.println("hello");
             }
         };
         Thread t1 = new Thread(obj1);
