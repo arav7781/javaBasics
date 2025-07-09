c7744bb65309a476cca6c77b9f41374f80485069
arav7781 <aravsaxena884@gmail.com>
2025-07-09 15:20:16 +0530

    methof reference ::

---
+++ Threads/methodRef.java
@@ -0,0 +1,26 @@
+package Threads;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class methodRef {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("arav", "vivek", "rohit");
+
+    // Using lambda
+    List<String> uNames = names.stream()
+        .map(name -> name.toUpperCase())
+        .toList();
+
+    // Alternatively, using method reference
+    // List<String> uNames = names.stream()
+    //                            .map(String::toUpperCase)
+    //                            .collect(Collectors.toList());
+
+    
+    System.out.println(uNames);
+    }
+                        
+
+}
